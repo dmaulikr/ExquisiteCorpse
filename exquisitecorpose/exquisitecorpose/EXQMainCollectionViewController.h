fc918d17b7460022460c79207479f17f81eb5c33
Reid van Melle <rvanmelle@gmail.com>
2013-10-19 16:36:42 -0400

    WIP -- main launch collection view

---
+++ exquisitecorpose/exquisitecorpose/EXQMainCollectionViewController.h
@@ -0,0 +1,13 @@
+//
+//  EXQMainCollectionViewController.h
+//  exquisitecorpose
+//
+//  Created by Reid van Melle on 2013-10-19.
+//  Copyright (c) 2013 Startup Weekend. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EXQMainCollectionViewController : UICollectionViewController
+
+@end
