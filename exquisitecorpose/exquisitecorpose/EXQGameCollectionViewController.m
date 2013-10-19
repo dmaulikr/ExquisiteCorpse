a8363853ec3e06b353cff7bfaa628f7be1a62b2c
Reid van Melle <rvanmelle@gmail.com>
2013-10-19 16:08:09 -0400

    delete old dev art

--- exquisitecorpose/exquisitecorpose/EXQGameCollectionViewController.m
+++ exquisitecorpose/exquisitecorpose/EXQGameCollectionViewController.m
@@ -9,7 +9,6 @@
 #import "EXQGameCollectionViewController.h"
 
 @interface EXQGameCollectionViewController () {
-    NSArray *recipeImages;
     NSArray *gameImages;
 }
 
@@ -23,7 +22,6 @@
     self.collectionView.backgroundColor = [UIColor whiteColor];
 	
     // Initialize recipe image array
-    recipeImages = [NSArray arrayWithObjects:@"angry_birds_cake.jpg", @"creme_brelee.jpg", @"egg_benedict.jpg", @"full_breakfast.jpg", @"green_tea.jpg", @"ham_and_cheese_panini.jpg", @"ham_and_egg_sandwich.jpg", @"hamburger.jpg", @"instant_noodle_with_egg.jpg", @"japanese_noodle_with_pork.jpg", @"mushroom_risotto.jpg", @"noodle_with_bbq_pork.jpg", @"starbucks_coffee.jpg", @"thai_shrimp_cake.jpg", @"vegetable_curry.jpg", @"white_chocolate_donut.jpg", nil];
     gameImages = @[@"download.jpeg",@"download1.jpeg",@"download2.jpeg",@"download3.jpeg",@"download5.jpeg",@"download6.jpeg", @"images.jpeg",@"images1.jpeg",@"images2.jpeg",@"images3.jpeg",@"images4.jpeg",@"images5.jpeg",@"images6.jpeg",@"images7.jpeg",@"images8.jpeg",@"images9.jpeg",@"images10.jpeg",@"images11.jpeg",@"images12.jpeg",@"images13.jpeg",@"images14.jpeg"];
 
 }
