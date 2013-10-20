02ba08fc624e263a8748335186194106187f640a
Josh Svatek <svatek@gmail.com>
2013-10-19 23:30:41 -0400

    Small project change hopefully reid has to fix merge of this time

---
+++ exquisitecorpose/exquisitecorpose/EXQScene.h
@@ -0,0 +1,13 @@
+//
+//  EXQScene.h
+//  exquisitecorpose
+//
+//  Created by Josh Svatek on 10/19/2013.
+//  Copyright (c) 2013 Startup Weekend. All rights reserved.
+//
+
+#import <SpriteKit/SpriteKit.h>
+
+@interface EXQScene : SKScene
+
+@end
